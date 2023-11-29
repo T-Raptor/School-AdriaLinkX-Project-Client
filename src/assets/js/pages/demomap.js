76803b74b5d13aca32acd2c4006e9fb6ac44dc47
Ahmad Kassam <ahmad.kassam@student.howest.be>
2023-11-29 15:37:57 +0100

    getting the map on the monitor screen, getting shuttles & warnings on the monitor screen

--- src/assets/js/pages/demomap.js
+++ src/assets/js/pages/demomap.js
@@ -122,3 +122,5 @@ function createTracksLayer() {
         })
     });
 }
+
+export {createMap};
---
+++ src/assets/js/pages/monitorscreen.js
@@ -0,0 +1,59 @@
+"use strict";
+
+import {createMap} from "./demomap.js";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    createMap();
+    displayWarnings();
+    displayShuttles();
+    
+
+}
+
+
+
+const warnings = ["Track Blocked At: \n Adria <-> Badria",
+                          "Track Blocked At: \n Adria <-> Cedria",
+                          "Some heavy wind expect in Badria",
+                          "Rain expected in Cedria",
+                          "Storm expected in Dadria"];
+
+const shuttles = [
+    "AE4-BSD-XES",
+    "AE5-DSD-XMD",
+    "AE6-DBD-XSM",
+    "AE8-DAB-FMX"
+];
+
+function displayWarnings() {
+
+
+    const warningList = document.querySelector("#notices");
+
+
+
+    for (let i = 0; i < 2; i++) {
+        const random =  warnings[Math.floor(Math.random() * warnings.length)];
+        warningList.insertAdjacentHTML("beforeend",`<ul>
+    <li>${random}</li>
+    <li class="material-icons">warning</li>
+    </ul>`
+        );}
+
+}
+
+function displayShuttles() {
+    const shuttlesList = document.querySelector("#shuttles");
+
+    for (let i = 0; i < 2; i++) {
+        const random =  shuttles[Math.floor(Math.random() * shuttles.length)];
+        shuttlesList.insertAdjacentHTML("beforeend",`<ul>
+    <li>${random}</li>
+    <li class="material-icons">train</li>
+    </ul>`
+        );}
+
+
+}
