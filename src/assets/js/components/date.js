667a8e5737eb16e3898b4edecf51562857849beb
Ahmad Kassam <ahmad.kassam@student.howest.be>
2023-12-06 14:40:18 +0100

    seprated the date picker code & edited the map and made small changes to the css and the html related to the map

---
+++ src/assets/js/components/date.js
@@ -0,0 +1,77 @@
+// date.js
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+const prevMonthBtn = document.querySelector('#prevMonth');
+const nextMonthBtn = document.querySelector('#nextMonth');
+const currentMonthYear = document.querySelector('#currentMonthYear');
+const selectedDateInput = document.querySelector('#selectedDate');
+const calendarBody = document.querySelector('#calendarBody');
+
+let currentDate = new Date(),
+    currentMonth = currentDate.getMonth(),
+    currentYear = currentDate.getFullYear();
+
+function init() {
+    renderCalendar();
+}
+
+
+function renderCalendar() {
+    calendarBody.innerHTML = '';
+    const firstDay = new Date(currentYear, currentMonth, 1),
+        lastDay = new Date(currentYear, currentMonth + 1, 0);
+
+    currentMonthYear.textContent = new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        year: 'numeric'
+    }).format(firstDay);
+
+    ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].forEach(day => calendarBody.innerHTML += `<div class="day">${day}</div>`);
+
+    for (let i = 1 - firstDay.getDay(); i <= lastDay.getDate(); i++) {
+        const dayElement = document.createElement('div');
+        dayElement.className = 'day';
+
+        if (i > 0) {
+            dayElement.textContent = i;
+            if (currentDate.getDate() === i && currentDate.getMonth() === currentMonth && currentDate.getFullYear() === currentYear) dayElement.classList.add('current-day');
+
+            // Add click event to capture selected date
+            dayElement.addEventListener('click', function (event) {
+                event.preventDefault();
+                const selectedDate = new Date(currentYear, currentMonth, i + 1);
+                const formattedDate = selectedDate.toISOString().split('T')[0];
+                selectedDateInput.value = formattedDate;
+            });
+        }
+
+        calendarBody.appendChild(dayElement);
+    }
+}
+
+renderCalendar();
+
+
+prevMonthBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    currentMonth--;
+    if (currentMonth < 0) {
+        currentMonth = 11;
+        currentYear--;
+    }
+    renderCalendar();
+});
+
+nextMonthBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    currentMonth++;
+    if (currentMonth > 11) {
+        currentMonth = 0;
+        currentYear++;
+    }
+    renderCalendar();
+});
+
+export { renderCalendar, prevMonthBtn, nextMonthBtn };
