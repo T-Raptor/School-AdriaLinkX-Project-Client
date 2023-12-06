64368574c5728c0f77a0eda0171c47b3f77cdb6d
Popal Ares <ares.popal@student.howest.be>
2023-12-06 19:26:58 +0100

    formatted code

--- src/assets/js/pages/monitor.js
+++ src/assets/js/pages/monitor.js
@@ -4,6 +4,7 @@ import { getStations } from "../api.js";
 
 
 document.addEventListener("DOMContentLoaded", init);
+
 function init() {
     const map = createMap("centra-map");
     fetchAndDrawStations(map);
@@ -23,10 +24,10 @@ function fetchAndDrawStations(map) {
 
 
 const warnings = ["Track Blocked At: \n Adria &lt;-&gt; Badria",
-                          "Track Blocked At: \n Adria &lt;-&gt; Cedria",
-                          "Some heavy wind expect in Badria",
-                          "Rain expected in Cedria",
-                          "Storm expected in Dadria"];
+    "Track Blocked At: \n Adria &lt;-&gt; Cedria",
+    "Some heavy wind expect in Badria",
+    "Rain expected in Cedria",
+    "Storm expected in Dadria"];
 
 const shuttles = [
     "AE4-BSD-XES",
@@ -39,12 +40,13 @@ function displayWarnings() {
     const warningList = document.querySelector("#notices");
 
     for (let i = 0; i < 2; i++) {
-        const random =  warnings[Math.floor(Math.random() * warnings.length)];
-        warningList.insertAdjacentHTML("beforeend",`<ul>
+        const random = warnings[Math.floor(Math.random() * warnings.length)];
+        warningList.insertAdjacentHTML("beforeend", `<ul>
     <li>${random}</li>
     <li class="material-icons">warning</li>
     </ul>`
-        );}
+        );
+    }
 
 }
 
@@ -52,8 +54,8 @@ function displayShuttles() {
     const shuttlesList = document.querySelector("#shuttles");
 
     for (let i = 0; i < 2; i++) {
-        const random =  shuttles[Math.floor(Math.random() * shuttles.length)];
-        shuttlesList.insertAdjacentHTML("beforeend",`<ul>
+        const random = shuttles[Math.floor(Math.random() * shuttles.length)];
+        shuttlesList.insertAdjacentHTML("beforeend", `<ul>
     <li>${random}</li>
     <li class="material-icons">train</li>
     </ul>`
