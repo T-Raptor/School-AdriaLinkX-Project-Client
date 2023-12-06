60079025454419452b4db0cbef7859f1e333ca8f
Buramuh <buramuh@gmail.com>
2023-12-06 01:08:28 +0100

    Seperate map component from demomap
    A lot of code is now depending on the demomap for some reason.
    
    This is very illogical. As such I'll do the work of seperating
    the generic map component from the page and moving links
    towards this module instead.

--- src/assets/js/pages/demomap.js
+++ src/assets/js/pages/demomap.js
@@ -1,59 +1,20 @@
 "use strict";
+import { createMap, drawStation } from "../components/map.js";
 
 document.addEventListener("DOMContentLoaded", init);
+function init() {
+    createMap("centra-map");
+    setInterval(moveAndUpdate, 50);
+}
 
 function moveAndUpdate() {
     applyRandomMovement();
     updateShuttles();
 }
 
-function init() {
-   createMap();
-   setInterval(moveAndUpdate, 50);
-}
 
-function createMap() {
-    const map = new ol.Map({
-        target: 'centra-map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.TileJSON({
-                    url: 'https://api.maptiler.com/maps/basic-v2/256/tiles.json?key=v7RhHG7ZxC29bKCh6207',
-                    tileSize: 256
-                })
-            })
-        ],
-        view: new ol.View({
-            center: ol.proj.fromLonLat([4.34878, 50.85045]),
-            zoom: 4
-        })
-    });
 
-    const coloniesLayer = createColoniesLayer();
-    const shuttlesLayer = createShuttlesLayer();
-    const tracksLayer = createTracksLayer();
-    map.addLayer(coloniesLayer);
-    map.addLayer(shuttlesLayer);
-    map.addLayer(tracksLayer);
 
-    return map;
-}
-
-function createColoniesLayer() {
-    return new ol.layer.Vector({
-        source: new ol.source.Vector({
-            url: 'assets/js/colonies.geojson',
-            format: new ol.format.GeoJSON()
-        }),
-        style: new ol.style.Style({
-            image: new ol.style.Icon({
-                src: 'assets/images/generic_marker.png',
-                tileSize: 256,
-                scale: 0.008
-            })
-        }),
-    });
-}
 
 const shuttles = [
     "AE6-C72-EFA",
@@ -67,31 +28,7 @@ const locShuttles = {
 
 
 const ftShuttles = {};
-function createShuttleSource() {
-    return new ol.source.Vector({
-        features: shuttles.map(shuttle => ftShuttles[shuttle])
-    });
-}
-function createShuttlesLayer() {
-    for (const shuttle of shuttles) {
-        ftShuttles[shuttle] = new ol.Feature(
-            new ol.geom.Point(
-                ol.proj.fromLonLat(locShuttles[shuttle])
-            )
-        );
-    }
 
-    return new ol.layer.Vector({
-        source: createShuttleSource(),
-        style: new ol.style.Style({
-            image: new ol.style.Icon({
-                src: 'assets/images/generic_marker.png',
-                tileSize: 256,
-                scale: 0.008
-            })
-        }),
-    });
-}
 
 
 function applyRandomMovement() {
@@ -108,19 +45,3 @@ function updateShuttles() {
         );
     }
 }
-
-
-function createTracksLayer() {
-    return new ol.layer.Vector({
-        source: new ol.source.Vector({
-            url: 'assets/js/tracks.geojson',
-            format: new ol.format.GeoJSON()
-        }),
-        style: new ol.style.Style({
-          fill: new ol.style.Fill({ color: '#FF0000', weight: 4}),
-          stroke: new ol.style.Stroke({ color: '#FF0000', weight: 2})
-        })
-    });
-}
-
-export {createMap};
