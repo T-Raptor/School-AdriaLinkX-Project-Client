1c79c3060870129f51212eff8f81b68d32ab0c8b
Vanhoorne Bram <bram.vanhoorne@student.howest.be>
2023-12-13 22:58:21 +0100

    Track selection status to enable toggling

--- src/assets/js/components/routepicker.js
+++ src/assets/js/components/routepicker.js
@@ -1,14 +1,31 @@
 const MARKER_TRACK_SELECTED_STYLE = new ol.style.Style({
-    fill: new ol.style.Fill({ color: "#00ff00", weight: 40}),
-    stroke: new ol.style.Stroke({ color: "#00ff00", weight: 40, width: 4})
+    fill: new ol.style.Fill({ color: "#00ff00" }),
+    stroke: new ol.style.Stroke({ color: "#00ff00", width: 4 })
 });
+
+
+function trackFeatureToEntity(map, feature) {
+    for(const entity of map.entities.tracks) {
+        if (entity.feature === feature) {
+            return entity;
+        }
+    }
+    return null;
+}
 
 function createRoutePicker(map) {
     const routePicker = {map: map, selection: []};
     map.olMap.on("click", function(e) {
         map.olMap.forEachFeatureAtPixel(e.pixel, function (feat, layer) {
             if (map.lyrTracks === layer) {
-                feat.setStyle(MARKER_TRACK_SELECTED_STYLE);
+                const entity = trackFeatureToEntity(map, feat);
+                if (entity.selected) {
+                    entity.selected = false;
+                    feat.setStyle();
+                } else {
+                    entity.selected = true;
+                    feat.setStyle(MARKER_TRACK_SELECTED_STYLE);
+                }
             }
         });
     });
