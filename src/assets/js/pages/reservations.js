1da3dd31296eecd3f8d98df3d52a1c4b33681ad3
Popal Ares <ares.popal@student.howest.be>
2023-12-16 01:53:31 +0100

    can now display all reservations from every company

--- src/assets/js/pages/reservations.js
+++ src/assets/js/pages/reservations.js
@@ -8,71 +8,63 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
     const map = createMap("centra-map");
     fetchAndDrawStationsAndTracks(map);
-    displayCompanyName();
     displayReservations();
 }
 
 
-const tempReservations = [
-    {
-        id: 274,
-        date: "22/11/2023",
-        start_time: "12:00",
-        timeframe: "03.00"
-    },
-    {
-        id: 275,
-        date: "23/11/2023",
-        start_time: "19:00",
-        timeframe: "02.00"
-    },
-    {
-        id: 276,
-        date: "25/11/2023",
-        start_time: "07:00",
-        timeframe: "09.00"
-    },
-    {
-        id: 277,
-        date: "26/11/2023",
-        start_time: "15:00",
-        timeframe: "01:30"
-    },
-    {
-        id: 278,
-        date: "27/11/2023",
-        start_time: "11:00",
-        timeframe: "04:15"
-    }
-];
-
 function displayReservations() {
-    const reservationsList = document.querySelector("#reservations");
+    getReservations(reservations => {
+        reservations.forEach(reservation => {
+            const currentReservation = {
+                id: reservation.id,
+                date: formatDate(reservation.periodStart),
+                start_time: formatTime(reservation.periodStart),
+                timeframe: formatTime(reservation.periodStop - reservation.periodStart)
+            };
 
-    if (tempGetLocalStorageReservation()) {
-        const locsto = JSON.parse(tempGetLocalStorageReservation());
-
-        reservationsList.insertAdjacentHTML("beforeend", `<ul data-id="localStorage">
-    <li>#justReserved</li>
-    <li>${locsto.date}</li>
-    <li>${locsto.time} + ${locsto.additionalTime}</li>
-    </ul>`
-        );
-    }
-
-    for (let i = 0; i < 2; i++) {
-        const random = tempReservations[Math.floor(Math.random() * tempReservations.length)];
-        reservationsList.insertAdjacentHTML("beforeend", `<ul data-id="${random.id}">
-    <li>#${random.id}</li>
-    <li>${random.date}</li>
-    <li>${random.start_time} + ${random.timeframe}</li>
-    </ul>`
-        );
-    }
+            console.log(reservation);
+            console.log(currentReservation);
+            displayReservationItems(currentReservation);
+        });
+    });
 }
 
-function setCompanyName(companyName) {
+function displayCompanyName(companyName) {
     const companyNameText = document.querySelector("#company-name");
 
     companyNameText.textContent = companyName;
+}
+
+function displayReservationItems(reservation) {
+    const reservationsHTMLList = document.querySelector("#reservations");
+
+    reservationsHTMLList.insertAdjacentHTML("beforeend",
+        `<ul data-id="${reservation.id}">
+            <li>#${reservation.id}</li>
+            <li>${reservation.date}</li>
+            <li>${reservation.start_time} + ${reservation.timeframe}</li>
+        </ul>`
+    );
+}
+
+function formatDate(timestamp) {
+    const date = new Date(timestamp);
+
+    return date.toLocaleString("nl-BE",
+        {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        });
+}
+
+function formatTime(timestamp) {
+    const time = new Date(timestamp);
+
+    return time.toLocaleString("nl-BE",
+        {
+            hourCycle: "h24",
+            hour: "2-digit",
+            minute: "2-digit"
+        });
 }
