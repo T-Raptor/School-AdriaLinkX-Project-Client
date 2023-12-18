1f7080ee3627bbcb7d73ba148285af91291445bc
Vanhoorne Bram <bram.vanhoorne@student.howest.be>
2023-12-18 02:28:47 +0100

    Turn map into a route picker on reservation page

--- src/assets/js/pages/reserve.js
+++ src/assets/js/pages/reserve.js
@@ -1,10 +1,12 @@
 "use strict";
 import { createCalendar } from "../components/date.js";
 import { createMap, fetchAndDrawStationsAndTracks } from "../components/map.js";
+import { createRoutePicker } from "../components/routepicker.js";
 
 
 document.addEventListener('DOMContentLoaded', function () {
     const map = createMap("centra-map");
+    createRoutePicker(map);
     fetchAndDrawStationsAndTracks(map);
 
     const reservationForm = document.querySelector('#reservationForm');
