07f382543d44db7ef4586a3d85ffb28a73f2449d
Popal Ares <ares.popal@student.howest.be>
2023-12-18 05:04:33 +0100

    resolved big merge conflicts and implemented shuttledisplay

--- src/assets/js/pages/monitor.js
+++ src/assets/js/pages/monitor.js
@@ -1,72 +1,98 @@
 "use strict";
+
 import { createMap, drawShuttle, fetchAndDrawStationsAndTracks, getEntity, updateShuttle } from "../components/map.js";
-import { getEventsWith } from "../api.js";
+import { getEvents } from "../api.js";
 
 
 document.addEventListener("DOMContentLoaded", init);
 
-function getLastMoveForId(events, id) {
-    let move = null;
-    for (const event of events) {
-        if (event.subject === "MOVE" && event.target.id === id) {
-            move = event;
-        }
-    }
-    return move;
-}
-
-function getUniqueIds(events) {
-    const ids = new Set();
-    for (const event of events) {
-        if (event.subject === "MOVE") {
-            ids.add(event.target.id);
-        }
-    }
-    return ids;
-}
-
 function init() {
     const map = createMap("centra-map");
     fetchAndDrawStationsAndTracks(map);
-    displayWarnings();
-    displayShuttles();
+    //displayWarnings();
+    renderShuttles(map);
 
-    fetchAndUpdateShuttles(map);
-    setInterval(() => fetchAndUpdateShuttles(map), 5000);
+    getEvents((events) => {
+        console.table(events);
+    });
+
 }
 
-function fetchAndUpdateShuttles(map) {
-    getEventsWith("?subject=MOVE", function(events) {
-        const ids = getUniqueIds(events);
-        for (const id of ids) {
-            const move = getLastMoveForId(events, id);
-            const ent = getEntity(map, "shuttles", id);
-            if (ent === null) {
-                drawShuttle(map, move.target.id, [move.latitude, move.longitude]);
-            } else {
-                updateShuttle(ent, [move.latitude, move.longitude]);
-            }
+// Shuttles
+function renderShuttles(map) {
+    fetchAndRenderShuttles(map);
+    setInterval(() => fetchAndRenderShuttles(map), 5000);
+}
+
+function fetchAndRenderShuttles(map) {
+    const movingShuttle = {
+        subject: 'MOVE',
+    };
+
+    getEvents((shuttles) => {
+        const shuttleList = prepareShuttleList(shuttles);
+        renderShuttleList(shuttleList);
+        drawAndUpdateShuttles(map, shuttles);
+    }, movingShuttle);
+}
+
+function prepareShuttleList(shuttles) {
+    return shuttles.map((shuttle) => ({
+        id: shuttle.id,
+        name: `Tempname #${Math.floor(Math.random() * 100)}`
+    }));
+}
+
+function renderShuttleList(shuttleList) {
+    const shuttleListElement = document.querySelector("#shuttles");
+
+    shuttleListElement.innerHTML = shuttleList.map((shuttle) => `
+            <ul data-id="${shuttle.id}">
+                <li>${shuttle.name}</li>
+            </ul>`
+    ).join("");
+}
+
+function drawAndUpdateShuttles(map, events) {
+    const uniqueIds = getUniqueIds(events);
+    uniqueIds.forEach((id) => {
+        const move = getLastMoveForId(events, id);
+        const ent = getEntity(map, "shuttles", id);
+
+        if (ent === null) {
+            drawShuttle(map, move.target.id, [move.latitude, move.longitude]);
+        } else {
+            updateShuttle(ent, [move.latitude, move.longitude]);
         }
     });
 }
 
+function getUniqueIds(events) {
+    return [...new Set(
+        events
+            .filter((event) => event.subject === "MOVE")
+            .map((event) => event.target.id)
+    )];
+}
+
+function getLastMoveForId(events, id) {
+    return events.find(
+        (event) => event.subject === "MOVE" && event.target.id === id
+    ) || null;
+}
 
 
 
-const warnings = ["Track Blocked At: \n Adria &lt;-&gt; Badria",
-    "Track Blocked At: \n Adria &lt;-&gt; Cedria",
-    "Some heavy wind expect in Badria",
-    "Rain expected in Cedria",
-    "Storm expected in Dadria"];
+// Notices
+function renderWarnings() {
+    return;
+}
 
-const shuttles = [
-    "AE4-BSD-XES",
-    "AE5-DSD-XMD",
-    "AE6-DBD-XSM",
-    "AE8-DAB-FMX"
-];
+function prepareWarningForList(warning) {
+    return;
+}
 
-function displayWarnings() {
+function renderWarningItem(warning) {
     const warningList = document.querySelector("#notices");
 
     for (let i = 0; i < 2; i++) {
@@ -78,17 +104,4 @@ function displayWarnings() {
         );
     }
 
-}
-
-function displayShuttles() {
-    const shuttlesList = document.querySelector("#shuttles");
-
-    for (let i = 0; i < 2; i++) {
-        const random = shuttles[Math.floor(Math.random() * shuttles.length)];
-        shuttlesList.insertAdjacentHTML("beforeend", `<ul>
-    <li>${random}</li>
-    <li class="material-icons">train</li>
-    </ul>`
-        );
-    }
 }
