94381008bac713c3c00ecca8f44ab508e2b41138
mohamed sharfuddeen nadeem <mohamed.sharfuddeen.nadeem@student.howest.be>
2023-12-19 10:28:51 +0100

    made the notifaction arrey so when there are multiple it shows one after other

--- src/assets/js/components/notification.js
+++ src/assets/js/components/notification.js
@@ -8,22 +8,34 @@ function init() {
 }
 
 function createNotification() {
-    // Let's check if the browser supports notifications
+    if ("Notification" in window) {
+        // Request permission to show notifications
+        Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                // Array to store notifications
+                let notifications = [];
 
+                // Function to create and display a notification
+                function createNotification(message) {
+                    return new Notification(message, {
+                        icon: "path/to/icon.png" // Replace with your own icon path
+                    });
+                }
 
-if ("Notification" in window) {
-    Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-            let notification = new Notification("Hello, World!", {
-                icon: "path/to/icon.png"
-            });
+                // Add notifications to the array
+                notifications.push(createNotification("Notification 1"));
+                notifications.push(createNotification("Notification 2"));
+                notifications.push(createNotification("Notification 3"));
 
-            notification.onclick = function () {
-                notification.close();
-            };
-        }
-    });
-} else {
-    console.log("This browser does not support notifications.");
-}
+                // Close the notification when it's clicked
+                notifications.forEach(function (notification) {
+                    notification.onclick = function () {
+                        notification.close();
+                    };
+                });
+            }
+        });
+    } else {
+        console.log("This browser does not support notifications.");
+    }
 }
