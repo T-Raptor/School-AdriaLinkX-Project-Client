75c71fc25ca5418312cc29922a67ed6e284a6da3
Popal Ares <ares.popal@student.howest.be>
2023-12-20 01:35:18 +0100

    using the index page, now redirects to reserve if the company does not have reservations yet

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -1,16 +1,56 @@
 "use strict";
 
+import { getReservations } from "../api.js";
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const companies = document.querySelectorAll("#company-list li");
+    const loginButton = document.querySelector("#log-in");
+    loginButton.addEventListener("click", login);
 
-    companies.forEach(li => li.addEventListener("click", addCompanySelector));
+    const companies = document.querySelectorAll("#company-list button");
+    companies.forEach(button => button.addEventListener("click", addCompanySelector));
+}
+
+function login(e) {
+    e.preventDefault();
+
+    const company = getSelectedCompany();
+    let foundReservation = false;
+
+    localStorage.setItem("companyName", company);
+
+    getReservations(reservations => {
+        reservations.forEach(reservation => {
+            if (reservation.company === company) {
+                foundReservation = true;
+            }
+        });
+
+        if (foundReservation || company === "Admin") {
+            window.location.href = "monitor.html";
+        } else {
+            window.location.href = "reserve.html";
+        }
+    });
 }
 
 function addCompanySelector(e) {
-    const companies = document.querySelectorAll("#company-list li");
+    e.preventDefault();
 
-    companies.forEach(li => li.classList.remove("active"));
-    e.closest(li).classList.add("active");
+    document.querySelectorAll("#company-list li").forEach(li => li.classList.remove("selected"));
+
+    e.target.closest("li").classList.add("selected");
+}
+
+function getSelectedCompany() {
+    const companies = document.querySelectorAll("#company-list li");
+    const selectedCompany = Array.from(companies).find(li => li.classList.contains("selected"));
+
+    if (selectedCompany) {
+        const companyName = selectedCompany.querySelector("button").innerText.trim();
+        return companyName;
+    } else {
+        return null;
+    }
 }
